c0cf362e06d9536f457e627f659c06c6533cfc66
xhrnca00 <89995247+xhrnca00@users.noreply.github.com>
2024-04-08 20:55:59 +0200

    Make 04 macros even better

--- 04/src/first.cpp
+++ 04/src/first.cpp
@@ -7,22 +7,31 @@ void print(const std::vector<int> &nums);
 void fill(std::vector<int> *nums);
 void sort(std::vector<int> *nums);
 
-#define FOR(name, pre, cond, post, block)      \
-    pre;                                       \
-    name##_BEG : if (!(cond)) goto name##_END; \
-    block post;                                \
-    goto name##_BEG;                           \
-    name##_END :;
+// as in: `for (pre; cond; post) { block; }`
+// name should be unique and without spaces
+#define FOR(name, pre, cond, post, block)          \
+    {                                              \
+        pre;                                       \
+        name##_BEG : if (!(cond)) goto name##_END; \
+        block;                                     \
+        post;                                      \
+        goto name##_BEG;                           \
+        name##_END :;                              \
+    }
 
-#define WHILE(name, cond, block)               \
-    name##_BEG : if (!(cond)) goto name##_END; \
-    block goto name##_BEG;                     \
-    name##_END :;
+// as in: `while (cond) { block; }`
+// name should be unique and without spaces
+#define WHILE(name, cond, block)                   \
+    {                                              \
+        name##_BEG : if (!(cond)) goto name##_END; \
+        block;                                     \
+        goto name##_BEG;                           \
+        name##_END :;                              \
+    }
 
 int main() {
     std::vector<int> nums(100);
     srand(time(nullptr));
-    print(nums);
     fill(&nums);
     print(nums);
     sort(&nums);
@@ -30,17 +39,43 @@ int main() {
     return 0;
 }
 
-void print(const std::vector<int> &nums) {
-    for (const auto &x : nums) {
-        std::cout << x << ", ";
+void print2(const std::vector<int> &nums) {
+    if (nums.empty()) {
+        std::cout << "Empty vector" << std::endl;
+        return;
     }
+    auto it = nums.begin();
+    std::cout << *it++;
+    WHILE(
+        PRINT2_L0, it != nums.end(),
+        std::cout << ", " << *it++
+    )
+    std::cout << std::endl;
+}
+
+void print(const std::vector<int> &nums) {
+    if (nums.empty()) {
+        std::cout << "Empty vector" << std::endl;
+        return;
+    }
+    auto it = nums.begin();
+    std::cout << *it++;
+PRINT_L0_BEG:
+    if (it == nums.end())
+        goto PRINT_L0_END;
+    std::cout << ", " << *it++;
+    goto PRINT_L0_BEG;
+PRINT_L0_END:
     std::cout << std::endl;
 }
 
 void fill2(std::vector<int> *const nums) {
     auto &ns = *nums;
-    FOR(FILL2_L0, int i = 0, i < ns.size(), ++i, ns[i] = rand() % 1000 + 1; std::cout << "HI\n";)
-    return;
+    FOR(
+        FILL2_L0, int i = 0, i < ns.size(), ++i,
+        ns[i] = rand() % 1000 + 1;
+        std::cout << "HI\n"
+    )
 }
 
 void fill(std::vector<int> *const nums) {
@@ -49,22 +84,22 @@ void fill(std::vector<int> *const nums) {
 FILL_L0_BEG:
     if (i >= ns.size())
         goto FILL_L0_END;
-
     ns[i] = rand() % 1000 + 1;
-
     i++;
     goto FILL_L0_BEG;
-
 FILL_L0_END:;
 }
 
 void sort2(std::vector<int> *nums) {
     auto &ns = *nums;
-    int j;
-    FOR(SORT2_L0, int i = 0, i < ns.size(), ++i,
-        FOR(SORT2_L1, j = 1, j < ns.size(), ++j,
+    FOR(
+        SORT2_L0, int i = 0, i < ns.size(), ++i,
+        FOR(
+            SORT2_L1, int j = 1, j < ns.size(), ++j,
             if (ns[j - 1] > ns[j])
-                std::swap(ns[j - 1], ns[j]);))
+                std::swap(ns[j - 1], ns[j]);
+        )
+    )
 }
 
 void sort(std::vector<int> *nums) {
@@ -85,6 +120,5 @@ SORT_L1_BEG:
 SORT_L1_END:
     i++;
     goto SORT_L0_BEG;
-SORT_L0_END:
-    return;
+SORT_L0_END:;
 }
